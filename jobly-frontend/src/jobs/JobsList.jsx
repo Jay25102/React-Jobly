7bca0824deecc648bd900573fe8347a6a4a1d6e2
Jay25102 <jasonhuang25102@gmail.com>
2024-09-08 15:06:44 -0400

    basic job listing

--- jobly-frontend/src/jobs/JobsList.jsx
+++ jobly-frontend/src/jobs/JobsList.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SearchForm from "../forms/SearchForm";
+import JoblyApi from "../../api";
+import JobCardList from "./JobCardList";
 
 function JobsList() {
 
+    const [jobs, setJobs] = useState(null);
+
+    useEffect(function fetchAllJobsOnMount() {
+        search();
+    }, []);
+    
+    async function search(title) {
+        let jobs = await JoblyApi.getJobs(title);
+        setJobs(jobs);
+    }
+
+    if (!jobs) return <div>loading...</div>
+
     return (
         <div>
-            jobs list
+            <SearchForm searchFor={search}/>
+            {jobs.length 
+                ? <JobCardList jobs={jobs}/>
+                : <p>Sorry, no results were found!</p>
+            }
         </div>
     )
 }
