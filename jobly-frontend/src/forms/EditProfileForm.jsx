2dd9ed0e84efb6f1f931b73661f1b260bb7d950d
Jay25102 <jasonhuang25102@gmail.com>
2024-09-08 14:57:37 -0400

    basic profileForm

--- jobly-frontend/src/forms/EditProfileForm.jsx
+++ jobly-frontend/src/forms/EditProfileForm.jsx
@@ -1,10 +1,94 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import JoblyApi from "../../api";
+import UserContext from "./UserContext";
 
 function EditProfileForm() {
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const [formData, setFormData] = useState({
+        firstName: currentUser.firstName,
+        lastName: currentUser.lastName,
+        email: currentUser.email,
+        username: currentUser.username,
+        password: "",
+    });
+    const [formErrors, setFormErrors] = useState([]);
+    const [saveConfirmed, setSaveConfirmed] = useState(false);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        let profileData = {
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            email: formData.email,
+            password: formData.password,
+        };
+        let username = formData.username;
+        let updatedUser;
+
+        try {
+            updatedUser = await JoblyApi.saveProfile(username, profileData);
+        } 
+        catch (errors) {
+            setFormErrors(errors);
+            return;
+        }
+
+        setFormData(data => ({ ...data, password: ""}));
+        setFormErrors([]);
+        setSaveConfirmed(true);
+        setCurrentUser(updatedUser);
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData(data => ({
+            ...data,
+            [name]: value,
+        }));
+        setFormErrors([]);
+    }
 
     return (
         <div>
-            edit profile form
+            <div>Profile</div>
+            <form>
+                <div>
+                    <label>First Name</label>
+                    <input
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <input
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Confirm password to make changes: </label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button onClick={handleSubmit}>Save Changes</button>
+                
+            </form>
         </div>
     )
 }
